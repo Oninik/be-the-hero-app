b9ae9c6143bda56a6ea1273874cb39a48592027b
Gabriel Pessoa Carvalheira <gabriel.carvalheiraibm.com@gabrielpc.local>
2020-03-29 18:19:42 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
+
+
+
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   // useState retorna um Array [valor, funcaoDeAtualizacao]
+
+//   function increment() {
+//     setCounter(counter + 1);
+//   }
+
+//   return (
+//     <div>
+//       <Header>Contador: {counter} </Header>
+//       <button onClick={increment}>Incrementar</button>
+//     </div>
+    
+//   );
+// }
 
 export default App;
